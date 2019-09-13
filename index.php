e7de227eb4f45f29495eed0ccff1cd8aff787ef8
alanmanderson <alanmanderson@gmail.com>
2019-09-13 17:34:33 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script type="text/javascript"></script>
+  <style type="text/css" media="all">@import url("main.css");</style>
+  <style type="text/css" media="all">@import url("http://images.z57websites.com/realestate/ae/css/openhouse.css");</style>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+
+<script language="JavaScript" src="http://images.z57websites.com/realestate/ae/js/script.js" type="text/javascript"></script>
+<script language="JavaScript" src="http://montananicelydunn.com/common/scripts/custom.js" type="text/javascript"></script>
+<!--[if IE 6]>
+  <style type="text/css">
+    img, div { behavior: url(images.z57websites.com/realestate/ae/css/iepngfix.htc) }
+  </style>
+<![endif]-->
+
+    <title>Cut Bank House For Sale By Owner - Dan and Judy Wendt</title>
+    <meta name="description" content="Troy,  real estate and homes for sale in Bull Lake and Libby. Your Troy  real estate resource center, find MLS listings, condos and homes for sale in Troy " />
+    <meta name="keywords" content="Cut Bank Real Estate, Cut Bank, Real Estate, Owner, For Sale, Homes, Wendt, Real Estate, Montana" />
+  </head>
+<body id="home">
+
+<div id="wrapper">
+	<div id="title">  
+		<span><a href="index.php" id="sitetitle" target="_top">Cut Bank Montana, House for Sale By Owner</a></span>
+	</div>
+
+  <div id="content">
+    <div id="welcome">
+      <div id="PositionContent" style="position: relative; left: 0px; top: 0px;">
+        <p>
+					<h3><a name="photos">Photos</a></h3>
+<p>
+We started coming&nbsp;to Lincoln County, Montana several years ago to visit family members and fell in love with the area.&nbsp; We ended up in 2002 buying a piece of vacant land and in the spring of 06 made the move from the high country of Colorado. <br />
+</p>
+<p class="MsoNormal">
+Our success is owed to the commitment we have in treating all people fairly and professionally.&nbsp; We have always been, and excelled, in customer service oriented jobs.&nbsp; We both love what we do and it shows. We are matchmakers; finding you the perfect property to buy or matching your property to the right buyer! <br />
+</p>
+					<h3><a name="Information">Information</a></h3>
+<p>
+Aside from real estate, we love hiking with our dog, spending as much time as we can in our beautiful and healthy forests or on the water.&nbsp; Call us today and you will see what it means when we say, &ldquo;taking service to a higher level&rdquo;. <br />
+</p>
+<p>
+<span>For Buyers; Please browse this website for all&nbsp;<font color="#3333ff"><a href="http://www.ashtondata.com/mls-idx-lin/index.cfm?qf=http://www.montananicelydunn.com" target="_blank"><font color="#3333ff">Lincoln County Multiple Listings.</font></a> &nbsp;</font>There is a great selection of inventory with&nbsp;very reasonable prices, something for everyone.&nbsp;&nbsp;Call or email us&nbsp;for further information on any of these listings.&nbsp; We show all listings and make our listings&nbsp;readily available to show as well! We now have a page on <a href="http://www.facebook.com/home.php?#%21/pages/Troy-MT/Nicely-Dunn-Real-Estate-in-NW-Montana/126264190751420" target="_blank"><font color="#3333ff">Facebook.</font></a> <br />
+
+</span><span>&nbsp;</span> 
+</p>
+<p>
+<span>For Sellers; Call for a&nbsp;free &amp; confidential<a href="http://www.montananicelydunn.com.z57preview.com/Troy_Montana_home_sales.shtml" target="_blank" title="Market Analysis"><font color="#3333ff"> </font><font color="#3333ff">Market Analysis</font></a> with absolutely no obligation. See what is for sale and what has sold in your neighborhood&nbsp;or area.&nbsp; Be an informed Seller!&nbsp;&nbsp;We offer beautiful Virtual Tours and are starting&nbsp;to&nbsp;create <a href="http://www.montananicelydunn.com.z57preview.com/custom2.shtml" target="_blank" title="Property Videos"><font color="#3333ff">property videos.</font></a> </span>
+
+</p>
+<span>
+<p>
+Please take a&nbsp;look at&nbsp;all&nbsp; <a href="http://mlsvendor.com/idx_tvlayout/mls-idx-lin/waterfront.cfm" target="_blank" title="Waterfront Properties"><font color="#3333ff">water front properties&nbsp;</font></a> in the area, including lakes, rivers, streams &amp; creeks. There are several <font color="#000000">water front </font><font color="#000000">properties </font>for sale on the <strong>Kootenai River</strong>, <strong>Yaak River</strong>, <strong>Bull Lake</strong> &amp; <strong>Thompson Lakes</strong>.&nbsp; WE show alll properties, not just our own!&nbsp; 
+
+</p>
+						<h3><a name="ContactUs">Contact Us</a></h3>
+<font color="#3366ff">
+	<font color="#000000">
+		<a href="http://www.cutbank.com" target="_blank" title="Community information">
+			<font color="#3333ff">
+				<u>community information,</u>
+			</font>
+		</a> 
+		<a href="http://www.montananicelydunn.com.z57preview.com/custom4.shtml" target="_blank" title="Local Resources">
+		</a>
+	</font>
+	<font color="#0099ff">
+		<font color="#000000">
+			<a href="http://www.montananicelydunn.com.z57preview.com/custom4.shtml" target="_blank" title="Local Resources">
+				<font color="#3333ff">
+					<u>local resources</u>
+				</font>
+			</a> 
+			<a href="http://www.montananicelydunn.com.z57preview.com/frame.shtml?http://www.onboardnavigator.com/WebContent/OBWC_Search.aspx?AID=47&amp;CD_SID=SC001&amp;AgentID=a40uz3vjlcnd" target="_blank" title="School Information">
+				<font color="#3333ff">
+					<u>school information</u> 
+				</font>
+			</a>
+		</font>
+	</font>
+</font>and more! This on-line property search utilizes the most up to date technology available in our 
+<font color="#0066ff">
+	<font color="#330000">area.
+	</font>
+	<br/>
+</font>
+<br/>
+</p>
+<p>
+Please bookmark this site for future reference and return often for all the latest property for sale in <strong>Troy, Libby, the Yaak</strong> and other <strong>Northwest Montana Real Estate</strong>! Thank you and we look forward to hearing from you.&nbsp; 
+
+</p>
+<p>
+<font color="#3333ff">Dan and Judy Wendt<br/>
+Cut Bank Montana<br/>
+(406)873-4555 OR (406) 781-4555</font>
+
+</p>
+</span>
+
+      </div>
+    </div>
+		
+  <div id="feature">
+	<!--<div id="ftr1">    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="200" height="150" id="slideshow" align="middle">
+    <param name="allowScriptAccess" value="sameDomain" />
+
+    <param name="movie" value="/listings/slideshow.swf?xml=/slideshow_std.xml&amp;links=false" />
+    <param name="loop" value="false" />
+    <param name="menu" value="false" />
+    <param name="quality" value="best" />
+    <param name="bgcolor" value="#ffffff" />
+    <embed src="/listings/slideshow.swf?xml=/slideshow_std.xml&amp;links=false" loop="false" menu="false" quality="best" bgcolor="#ffffff" width="200" height="150" name="slideshow" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+    </object>
+  <br /><br /><p></p><p></p>
+</div>--> <!-- <div id="ftr2">    <h3 id="featuredprop">Featured Properties</h3>
+
+    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="200" height="150" id="slideshow" align="middle">
+    <param name="allowScriptAccess" value="sameDomain" />
+    <param name="movie" value="/listings/slideshow.swf?xml=/listings/slideshow_featured.xml&amp;links=true" />
+    <param name="loop" value="false" />
+    <param name="menu" value="false" />
+    <param name="quality" value="best" />
+    <param name="bgcolor" value="#ffffff" />
+    <embed src="/listings/slideshow.swf?xml=/listings/slideshow_featured.xml&amp;links=true" loop="false" menu="false" quality="best" bgcolor="#ffffff" width="200" height="150" name="slideshow" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+    </object>
+
+    </div>-->
+
+</div><script type="text/javascript">
+<!--
+window.onload = function() {
+    var feature = document.getElementById('feature').clientHeight;
+    var welcome = document.getElementById('welcome').clientHeight;
+    if(feature > welcome)
+    {
+      newH = feature-welcome+20;
+      document.getElementById('content').style.paddingBottom = newH+'px';
+    }  }
+-->
+</script>
+
+
+
+
+  </div>
+
+  <div id="info">
+
+    <div id="photo">        <a href="/Tracy_Nicely_REALTOR.shtml"><img src="http://root.z57.com/filemanager/uploads/7/c/7c9f15e8-d869-b7d5-0d554bbf0a312705.JPG" width="145" height="134" border="0" alt="Tracy Nicely" /></a>
+
+              </div>
+
+    <div id="contact">    
+    <ul>
+      <li><h2>Tracy Nicely</h2></li>        <li>Broker/Owner, e-PRO</li>
+            <li>406-295-4600</li>              <li><a href="/realtor_contact.shtml">Email Us</a></li>
+                              </ul>      <ul>
+
+        <li><h2>Michael Dunn</h2></li>          <li>Assistant</li>
+                  <li>406-295-4600</li>
+        
+          <li><a href="/realtor_contact.shtml">Email Us</a></li>
+              </ul>
+        </div>
+
+    <div id="logo">
+
+    </div>
+
+  </div>    
+	
+<div id="nav">
+   <ul id="navlist">
+			<li id="nav1"><a href="index.php#Photos"><span>Pictures</span></a></li>
+      <li id="nav2"><a href="index.php#Information"><span>Information</span></a></li>
+      <li id="nav3"><a href="index.php#ContactUs"><span>Contact Us</span></a></li>
+      <li id="nav4"><a href="index.php#LocalResources"><span>Local Resources</span></a></li>
+      <li id="nav5"><a href="index.php#ContactUs"><span>Contact Us</span></a></li>
+   </ul> 
+</div>
+
+    
+<div id="footer">
+
+  <div id="misc">
+    <ul>
+
+    <li><a href="index.php" target="_blank">Top</a></li>
+    <li><a href="index.php" target="_blank">Second Link</a></li>    </ul>
+  </div>
+	
+
+<div id="AE|PC|F|F|F|F|F|SLIDE_STD|SLIDE_FEAT|F|F|F|T|F"></div>
+
+</div> <!--end footer div-->
+
+</div> <!--end wrapper div-->
+<script language="JavaScript" src="http://images.z57websites.com/realestate/ae/js/gaScript.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+if (typeof calcGAPagename == 'function') {
+      calcGAPagename();
+}
+var pageTrackerZ = _gat._getTracker("UA-470352-4");
+pageTrackerZ._setVar("AE|PC|F|F|F|F|F|SLIDE_STD|SLIDE_FEAT|F|F|F|T|F");
+pageTrackerZ._trackPageview(ga_pagename);
+
+</script>
+
+</body>
+</html>
+
+  
